662b7e4429d7936194f6f83fb8ccc399aab1e680
moseoh <azqazq195@gmail.com>
2023-05-01 03:49:45 +0900

    feat: freemarker convert util 추가

--- src/main/kotlin/com/moseoh/programmers_helper/_common/Utils.kt
+++ src/main/kotlin/com/moseoh/programmers_helper/_common/Utils.kt
@@ -1,5 +1,10 @@
 package com.moseoh.programmers_helper._common
 
+import com.moseoh.programmers_helper.actions.import_problem.model.dto.SolutionDto
+import freemarker.template.Configuration
+import freemarker.template.Template
+import java.io.StringWriter
+
 class Utils {
     companion object {
         fun convert(template: String, values: Map<String, String>): String {
@@ -8,6 +13,16 @@ class Utils {
                 result = result.replace(key, value)
             }
             return result
+        }
+
+        fun convert(templatePath: String, solutionDto: SolutionDto): String {
+            val configuration = Configuration(Configuration.DEFAULT_INCOMPATIBLE_IMPROVEMENTS)
+            configuration.setClassLoaderForTemplateLoading(Utils::class.java.classLoader, "/templates")
+            configuration.defaultEncoding = "UTF-8"
+            val template: Template = configuration.getTemplate(templatePath)
+            val writer = StringWriter()
+            template.process(mapOf("solutionDto" to solutionDto, "packagePath" to "packagePath"), writer)
+            return writer.toString()
         }
     }
 }
