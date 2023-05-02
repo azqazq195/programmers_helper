4953741ebe4346937a875eccdc542ff462f4aa3a
moseoh <azqazq195@gmail.com>
2023-05-02 11:21:53 +0900

    refactor: 오류 처리 수정 및 Custom Exception 추가

---
+++ src/main/kotlin/com/moseoh/programmers_helper/_common/CException.kt
@@ -0,0 +1,3 @@
+package com.moseoh.programmers_helper._common
+
+class CException(message: String) : RuntimeException(message)
--- src/main/kotlin/com/moseoh/programmers_helper/actions/import_problem/service/ParseService.kt
+++ src/main/kotlin/com/moseoh/programmers_helper/actions/import_problem/service/ParseService.kt
@@ -1,6 +1,7 @@
 package com.moseoh.programmers_helper.actions.import_problem.service
 
 import com.intellij.openapi.components.Service
+import com.moseoh.programmers_helper._common.CException
 import com.moseoh.programmers_helper.actions.import_problem.service.dto.ProblemDto
 import com.moseoh.programmers_helper.settings.model.ProgrammersHelperSettings
 import org.jsoup.nodes.Document
@@ -47,12 +48,15 @@ class ParseService(
         val tableHeaders = tableElement.select("thead > tr > th")
         val tableRows = tableElement.select("tbody > tr")
 
-        return tableRows.map { row ->
+        val list = tableRows.map { row ->
             val rowValues = row.select("td")
             rowValues.mapIndexed { index, td ->
                 tableHeaders[index].text() to td.text().toString()
             }.toMap()
         }.toList()
+
+        if (list.isEmpty()) throw CException("테스트 케이스의 값을 읽지 못했습니다.")
+        return list
     }
 
     private fun parseAnswerName(document: Document): String {
@@ -67,6 +71,8 @@ class ParseService(
             document.select("h5:containsOwn(입출력 예), h5:has(strong:containsOwn(입출력 예))").first()
                 ?: document.select("h3:containsOwn(입출력 예제), h3:has(strong:containsOwn(입출력 예제))").first()
                 ?: document.select("h3:containsOwn(예제 입출력), h3:has(strong:containsOwn(예제 입출력))").first()
-        return h5Element!!.nextElementSibling()!!
+
+        if (h5Element!!.nextElementSibling() == null) throw CException("테스트 케이스를 찾을 수 없습니다.")
+        return h5Element.nextElementSibling()!!
     }
 }
