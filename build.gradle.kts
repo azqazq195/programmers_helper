b4f9b706ce1c5ede77d9709f17e8d04438baf4de
moseoh <azqazq195@gmail.com>
2023-08-24 19:16:38 +0900

    refactor: support 2023.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.8.20"
-    id("org.jetbrains.intellij") version "1.14.2"
+    id("org.jetbrains.kotlin.jvm") version "1.9.0"
+    id("org.jetbrains.intellij") version "1.15.0"
 }
 
 group = "com.moseoh"
-version = "0.0.11"
+version = "0.0.12"
 
 repositories {
     mavenCentral()
@@ -21,7 +21,7 @@ dependencies {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2023.1.3")
+    version.set("2023.2.1")
     type.set("IC") // Target IDE Platform
 
     plugins.set(listOf(/* Plugin Dependencies */))
